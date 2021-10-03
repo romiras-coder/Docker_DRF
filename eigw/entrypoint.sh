890442e4b446122d9ecd137c6e7931a06a7674da
Moskotlinov Roman <rom_iras@mail.ru>
2021-10-03 23:25:51 +0300

    start

---
+++ eigw/entrypoint.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# wait for Postgres to start
+#sleep 10
+
+python manage.py makemigrations --no-input
+
+python manage.py migrate --no-input
+
+#python manage.py runserver 0.0.0.0:8000
+exec gunicorn eigw.wsgi:application -b 0.0.0.0:8000 --reload
