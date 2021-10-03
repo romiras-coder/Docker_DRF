f4289257f5b56fee1e323f0cca31a6518c7aa1a9
Moskotlinov Roman <rom_iras@mail.ru>
2021-10-04 01:56:05 +0300

    all done on django 2

--- eigw/entrypoint.sh
+++ eigw/entrypoint.sh
@@ -1,11 +1,13 @@
-#! /bin/bash
+#!/bin/bash
 
 # wait for Postgres to start
-sleep 10
+sleep 5
 
 python manage.py makemigrations --no-input
 
 python manage.py migrate --no-input
+
+python manage.py collectstatic --no-input
 
 #python manage.py runserver 0.0.0.0:8000
 exec gunicorn eigw.wsgi:application -b 0.0.0.0:8000 --reload
